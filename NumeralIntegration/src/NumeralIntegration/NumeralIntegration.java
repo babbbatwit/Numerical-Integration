64cc211a963808ab8f8738c3da4a95869ac421b6
Brenton Babb babbb@wit.edu <thebubfusion@gmail.com>
2021-11-01 18:27:30 -0400

    inital

---
+++ NumeralIntegration/src/NumeralIntegration/NumeralIntegration.java
@@ -0,0 +1,16 @@
+package NumeralIntegration;
+import java.util.Scanner;
+
+public class NumeralIntegration {
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("Please input the lower limit of the integral(a): ");
+		int lowerLimit = myScanner.nextInt();
+		
+		System.out.println("Please input the upper limit of the integral(b): ");
+		int upperLimit = myScanner.nextInt();
+		
+		System.out.
+	}
+}
