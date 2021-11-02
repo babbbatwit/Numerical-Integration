d9ec742570559b663a2d4eec1f0502659fae229c
Brenton Babb babbb@wit.edu <thebubfusion@gmail.com>
2021-11-01 20:29:09 -0400

    Calculations

--- NumeralIntegration/src/NumeralIntegration/NumeralIntegration.java
+++ NumeralIntegration/src/NumeralIntegration/NumeralIntegration.java
@@ -6,10 +6,10 @@ public class NumeralIntegration {
 		Scanner myScanner = new Scanner(System.in);
 		
 		System.out.print("Please input the lower limit of the integral(a): ");
-		int lowerLimit = myScanner.nextInt();
+		double lowerLimit = myScanner.nextInt();
 		
 		System.out.print("Please input the upper limit of the integral(b): ");
-		int upperLimit = myScanner.nextInt();
+		double upperLimit = myScanner.nextInt();
 		
 		System.out.print("Enter the number of subintervals (n): ");
 		int subintervals = myScanner.nextInt();
@@ -33,5 +33,59 @@ public class NumeralIntegration {
 		
 		System.out.print("F: ");
 		double F = myScanner.nextDouble();
+		
+		double deltaX = (upperLimit - lowerLimit) / subintervals;
+		
+		
+		double actualUpper = (A * Math.pow(upperLimit, 6))/ 6 + (B * Math.pow(upperLimit, 5))/ 5
+				+ (C * Math.pow(upperLimit, 4))/ 4 + (D * Math.pow(upperLimit, 3))/ 3 
+				+ (E * Math.pow(upperLimit, 2))/ 2 + F * upperLimit;
+		
+		double actualLower = (A * Math.pow(lowerLimit, 6))/ 6 + (B * Math.pow(lowerLimit, 5))/ 5
+				+ (C * Math.pow(lowerLimit, 4))/ 4 + (D * Math.pow(lowerLimit, 3))/ 3 
+				+ (E * Math.pow(lowerLimit, 2))/ 2 + F * lowerLimit;
+		
+		double actualFinal = actualUpper - actualLower;
+		
+		double midpointSum = 0;
+		double currentX = deltaX/2;
+		
+		//midpoint for loop
+		for(int i = 0; i < subintervals; i++) {
+			
+			midpointSum += A * Math.pow(currentX, 5) * deltaX + B * Math.pow(currentX, 4) * deltaX 
+					+ C * Math.pow(currentX, 3)  * deltaX +  D * Math.pow(currentX, 2) * deltaX 
+					+ E * currentX * deltaX + F * deltaX;
+			
+			currentX += deltaX;
+			
+		}
+		
+		double previousValue = 0;
+		double currentValue = 0;
+		double trapizoidSum = 0;
+		currentX = lowerLimit;
+		for(int i = 0; i <= subintervals; i++) {
+			//System.out.println(currentX);
+			if(i == 0) {
+				previousValue = A * Math.pow(currentX, 5) + B * Math.pow(currentX, 4)
+						+ C * Math.pow(currentX, 3) +  D * Math.pow(currentX, 2)  
+						+ E * currentX + F;
+				currentX += deltaX;
+			}
+			else {
+				currentValue = A * Math.pow(currentX, 5) + B * Math.pow(currentX, 4)
+						+ C * Math.pow(currentX, 3) +  D * Math.pow(currentX, 2) 
+						+ E * currentX + F;
+				trapizoidSum += ((currentValue + previousValue) / 2) * deltaX;
+				previousValue = currentValue;
+				currentX += deltaX;
+			}
+		}
+		double simpsonsRule = (2 * midpointSum + trapizoidSum)/3;
+		System.out.println(midpointSum);
+		System.out.println(trapizoidSum);
+		System.out.println(simpsonsRule);
+		System.out.println(actualFinal);
 	}
 }
